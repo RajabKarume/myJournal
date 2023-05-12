c649bb4fdd52216b3e15897b9a9d446afe8de5a9
Rajab Karume <rkarume@gmail.com>
2023-05-12 11:30:21 +0300

    ✨ added protected routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import './App.css';
 import SignUp from './components/Login/SignUp';
 import MainPage from './components/Mainpage/Display/MainPage';
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Login from './components/Login/Login';
+import LogIn from './components/Login/Login';
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+import { useContext } from 'react';
+import { AuthContext } from './components/Auth/WithAuth';
 
 function App() {
 
- 
+  const {currentUser} = useContext(AuthContext)
+  const ProtectedRoute = ({children}) => {
+    if (!currentUser){
+      return (
+        <Navigate to='/login' />
+      )
+    }
+    return children
+  }
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path={"/login"} element={<Login/>}  />
+          <Route path={"/login"} element={<LogIn/>}  />
           <Route path={"/signup"} element={<SignUp/>}  />
-          <Route path={'/'} element={ <MainPage/> } />
+          <Route path={'/'} element={<ProtectedRoute> <MainPage/> </ProtectedRoute>} />
         </Routes>
       </BrowserRouter>      
     </div>
